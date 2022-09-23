4455f42cd1904a2fb3d3a323a5ba1cf86c0374d0
trungtranghoang <trungtranghoang@gmail.com>
2022-09-23 15:08:37 +0700

    Connect Wallet

--- src/components/MeataMaskButton/MetaMaskButton.js
+++ src/components/MeataMaskButton/MetaMaskButton.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box, Button } from '@material-ui/core';
 
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
@@ -7,6 +7,7 @@ import axios from 'axios';
 import { connect, useDispatch } from 'react-redux';
 import { importNativeBalanceAction, importSignatureAction, disconnectWalletAction } from '../../lib/redux/actions/MoralisAction';
 import ShortButton from '../ShortButton';
+import { ethers } from 'ethers';
 
 const MetaMaskButton = (props) => {
 
@@ -16,6 +17,52 @@ const MetaMaskButton = (props) => {
     const { disconnectAsync } = useDisconnect();
     const { isConnected } = useAccount();
     const { signMessageAsync } = useSignMessage();
+
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [account, setAccount] = useState(null);
+    const [balance, setBalance] = useState(null);
+
+    useEffect(() => {
+        if (window.ethereum) {
+            window.ethereum.on('accountsChanged', accountsChanged);
+            window.ethereum.on('chainChanged', chainChanged);
+        }
+    }, []);
+
+    const connectHandler = async () => {
+        if (window.ethereum) {
+            try {
+                const res = await window.ethereum.request({
+                    method: 'eth_requestAccounts',
+                });
+                await accountsChanged(res[0]);
+            } catch (err) {
+                console.error(err);
+                setErrorMessage('There was a problem connecting to MetaMask');
+            }
+        } else {
+            setErrorMessage('Install MetaMask');
+        }
+    };
+
+    const accountsChanged = async (newAccount) => {
+        setAccount(newAccount);
+        console.log(`account: ${newAccount}`);
+
+        try {
+            dispatch(importNativeBalanceAction(account));
+        } catch (err) {
+            console.error(err);
+            setErrorMessage('There was a problem connecting to MetaMask');
+        }
+
+    };
+
+    const chainChanged = () => {
+        setErrorMessage(null);
+        setAccount(null);
+        setBalance(null);
+    };
 
     const onConnectWallet = async (e) => {
         e.preventDefault();
@@ -73,7 +120,7 @@ const MetaMaskButton = (props) => {
         <Box>
             {
                 !props.signature ?
-                    <ShortButton label={'CONNECT'} bgColor={'#007aff'} onClick={onConnectWallet} />
+                    <ShortButton label={'CONNECT'} bgColor={'#007aff'} onClick={connectHandler} />
                     :
                     <ShortButton label={'DISCONNECT'} bgColor={'#007aff'} onClick={onDisconnect} />
             }
