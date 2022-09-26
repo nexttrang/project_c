62811202819bce7c4376e21de3c7724efc4b0bd8
trungtranghoang <trungtranghoang@gmail.com>
2022-09-26 12:47:15 +0700

    revert metamask button

--- src/lib/helper/checker.js
+++ src/lib/helper/checker.js
@@ -6,12 +6,7 @@ const isStrongPassword = (password) => {
     return password.length > 5;
 };
 
-const isConnectorNotFoundError = (str) => {
-    return str.includes('ConnectorNotFoundError');
-};
-
 export default {
     isValidEmail,
     isStrongPassword,
-    isConnectorNotFoundError,
 };
