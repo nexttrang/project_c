6d5f69626c77bbd3479b18bf241d84dee52f7b8c
trungtranghoang <trungtranghoang@gmail.com>
2022-09-26 13:18:21 +0700

    Update Metamask

--- src/components/MeataMaskButton/MetaMaskButton.js
+++ src/components/MeataMaskButton/MetaMaskButton.js
@@ -7,6 +7,10 @@ import axios from 'axios';
 import { connect, useDispatch } from 'react-redux';
 import { importNativeBalanceAction, importSignatureAction, disconnectWalletAction } from '../../lib/redux/actions/MoralisAction';
 import ShortButton from '../ShortButton';
+import checker from '../../lib/helper/checker';
+
+const dappUrl = 'king-prawn-app-jg2si.ondigitalocean.app';
+const metamaskAppDeepLink = 'https://metamask.app.link/dapp/' + dappUrl;
 
 const MetaMaskButton = (props) => {
 
@@ -20,48 +24,55 @@ const MetaMaskButton = (props) => {
     const onConnectWallet = async (e) => {
         e.preventDefault();
 
-        if (isConnected) {
-            await disconnectAsync();
+        try {
+            if (isConnected) {
+                await disconnectAsync();
+            }
+
+            const { account, chain } = await connectAsync({
+                connector: new MetaMaskConnector()
+            });
+
+            const userData = {
+                data: {
+                    address: account,
+                    chain: chain.id,
+                }
+            };
+
+            console.log(userData);
+
+            const { data } = await axios.post(
+                'https://us-central1-tinderclone-ece49.cloudfunctions.net/v1/user/request_message',
+                userData,
+                {
+                    headers: {
+                        'content-type': 'application/json',
+                    },
+                }
+            );
+
+            console.log(`Received Message From Moralis: ${data}`);
+
+            const message = data.message;
+            const signature = await signMessageAsync({ message });
+
+            const verification = {
+                data: {
+                    message: message,
+                    signature: signature,
+                }
+            };
+
+            console.log(`verification ${JSON.stringify(verification)}`);
+            dispatch(importSignatureAction(signature));
+            dispatch(importNativeBalanceAction(account));
+        } catch (error) {
+            if (checker.isConnectorNotFoundError(JSON.stringify(error))) {
+                console.log('Please install MetaMask!');
+                window.open(metamaskAppDeepLink, '_blank');
+            }
         }
-
-        const { account, chain } = await connectAsync({
-            connector: new MetaMaskConnector()
-        });
-
-        const userData = {
-            data: {
-                address: account,
-                chain: chain.id,
-            }
-        };
-
-        console.log(userData);
-
-        const { data } = await axios.post(
-            'https://us-central1-tinderclone-ece49.cloudfunctions.net/v1/user/request_message',
-            userData,
-            {
-                headers: {
-                    'content-type': 'application/json',
-                },
-            }
-        );
-
-        console.log(`Received Message From Moralis: ${data}`);
-
-        const message = data.message;
-        const signature = await signMessageAsync({ message });
-
-        const verification = {
-            data: {
-                message: message,
-                signature: signature,
-            }
-        };
-
-        console.log(`verification ${JSON.stringify(verification)}`);
-        dispatch(importSignatureAction(signature));
-        dispatch(importNativeBalanceAction(account));
     };
 
     const onDisconnect = (e) => {
--- src/lib/helper/checker.js
+++ src/lib/helper/checker.js
@@ -6,7 +6,12 @@ const isStrongPassword = (password) => {
     return password.length > 5;
 };
 
+const isConnectorNotFoundError = (str) => {
+    return str.includes('ConnectorNotFoundError');
+};
+
 export default {
     isValidEmail,
     isStrongPassword,
+    isConnectorNotFoundError,
 };
